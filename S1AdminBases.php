74c7f52f36680b8160da3eaad8f7dfe8d663b60d
LuisLopez <lilc1105@gmail.com>
2023-08-14 04:50:59 -0600

    VBETA
    Funcional Pages, changes in table with radio columns and scripts

--- S1AdminBases.php
+++ S1AdminBases.php
@@ -46,6 +46,23 @@
 <header>
   
 </header>
+<script>
+        function Reassign() {
+            var a = document.getElementsByName("reassignChecks");
+            var b = document.getElementById("cb");
+            if (b.checked==true) {
+                for (var i = 0; i < a.length; i++) {
+                    a[i].disabled = true;
+                }
+            }
+            if (b.checked == false) {
+                for (var i = 0; i < a.length; i++) {
+                    a[i].disabled = false;
+                }
+            }
+        }
+    </script>
+
   <body>
     <!-- A vertical navbar -->
     <div class="navbar-container">
@@ -113,16 +130,22 @@
                 </tr>
               
               <?php
+              $contador = 0;
                 if ($result->num_rows > 0) {
                     while ($row = $result->fetch_assoc()) {
+                      $contador++; // Incrementamos el contador en cada iteración
+                      $id = "radius" . $contador; 
                       echo <<<HTML
                       <tr>
                           <td>{$row["process_name"]}</td>
                           <td>{$row["process_details"]}</td>
                           <td>{$row["question"]}</td>
-                          <td><input type="checkbox"></td>
-                          <td><input type="checkbox"></td>
-                          <td><input type="checkbox"></td>
+                          
+                          <td><input value = "si" name = "{$id}" type="Radio"></td>
+                          <td><input value = "no" name = "{$id}" type="Radio"></td>
+                          <td><input value = "na" name = "{$id}" type="Radio"></td>
+                          
+                          
                       </tr>
                       HTML;
                       }
@@ -133,7 +156,7 @@
               </table>
             </div>
             <div class="d-flex justify-content-md-center">
-              <button class="btn btn-outline-success" id="calculateColorBtn" type="submit">Enviar</button>
+              <button id="calculateColorBtn" class="btn btn-outline-success" type="button">Enviar</button>
               <button class="btn btn-outline-danger mx-2" id="limpiarBtn" type="submit">Limpiar</button>
             </div>
           </div>
--- S1Adminbases.js
+++ S1Adminbases.js
@@ -1,39 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("limpiarBtn").addEventListener("click", function() {
-        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(function(checkbox) {
-            checkbox.checked = false;
+        var radioButtons = document.querySelectorAll('input[type="radio"]');
+        radioButtons.forEach(function(radioButton) {
+            radioButton.checked = false;
         });
     });
 });
-document.getElementById("calculateColorBtn").addEventListener("click", function() {
-    let columnNumber = 4; // Número de la columna a calcular
-    let checkboxes = document.querySelectorAll(`td:nth-child(${columnNumber}) input[type="checkbox"]`);
-    let totalCheckboxes = checkboxes.length;
-    let checkedCheckboxes = 0;
 
-    checkboxes.forEach(function(checkbox) {
-        if (checkbox.checked) {
-            checkedCheckboxes++;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("calculateColorBtn").addEventListener("click", function() {
+        let columnNumber = 4; // Número de la columna a calcular
+        let rows = document.querySelectorAll('table tr:not(:first-child)'); // Obtener todas las filas excepto la primera
+
+        let totalRadioButtons = 0;
+        let checkedRadioButtons = 0;
+
+        rows.forEach(function(row) {
+            let radioButtons = row.querySelectorAll(`td:nth-child(${columnNumber}) input[type="radio"]`);
+
+            let selectedRadioButton = Array.from(radioButtons).find(radioButton => radioButton.checked);
+            if (selectedRadioButton && selectedRadioButton.value === 'si') {
+                checkedRadioButtons++;
+            }
+
+            totalRadioButtons += radioButtons.length; // Mover esta línea aquí
+        });
+
+        let percentage = totalRadioButtons > 0 ? (checkedRadioButtons / totalRadioButtons) * 100 : 0;
+        let color = 'red';
+
+        if (percentage >= 80) {
+            color = 'green';
+        } else if (percentage >= 30) {
+            color = 'yellow';
+        } else {
+            color = 'red';
         }
+
+        console.log("Color:", color);
+
+        setImageByColor(color);
     });
-
-    let percentage = (checkedCheckboxes / totalCheckboxes) * 100;
-    let color;
-
-    if (percentage >= 80) {
-        color = 'green'; 
-    } else if (percentage >= 30) {
-        color = 'yellow'; 
-    } else {
-        color = 'red'; 
-    }
-
-    console.log("Color:", color);
-
-    setImageByColor(color);
 });
-
 function setImageByColor(color) {
     let imageName = "";
 
